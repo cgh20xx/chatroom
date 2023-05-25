b53ffa8cb1a0bbfa8558a71cc2b6d3dad0f5b3d6
Hank <cgh20xx@gmail.com>
2023-05-25 10:51:33 +0800

    feat: 建立 server side socket 連線

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import devServer from './server/dev';
 import prodServer from './server/prod';
 import express from 'express';
+// import socket.io 和 http
+import { Server } from 'socket.io'
+import http from 'http'
 
 import { name } from '@/utils';
 
 const port = 3000;
 const app = express();
+// 建立 http server，傳入 express 的 app
+const server = http.createServer(app)
+// 建立 socket.io Server
+const io = new Server(server)
+
+io.on('connection',  (socket) => {
+  socket.emit('join', 'welcome')
+})
 
 // 執行npm run dev本地開發 or 執行npm run start部署後啟動線上伺服器
 if (process.env.NODE_ENV === 'development') {
@@ -16,6 +27,7 @@ if (process.env.NODE_ENV === 'development') {
 
 console.log('server side', name);
 
-app.listen(port, () => {
+// 將 app.listen 改為 server.listen
+server.listen(port, () => {
   console.log(`The application is running on port ${port}.`);
 });
