e2c3d7c9c612afe69a6878a2c4d146361ababd1c
Hank <cgh20xx@gmail.com>
2023-05-26 10:43:19 +0800

    feat: 新增房間頁接收網址參數

--- src/client/pages/chatRoom/index.ts
+++ src/client/pages/chatRoom/index.ts
@@ -1,6 +1,16 @@
 import './index.css';
 import { io } from 'socket.io-client'
 
+// 取得網址  queryString 的使用者名稱及房間名稱
+const url = new URL(location.href);
+const userName = url.searchParams.get('user_name')
+const roomName = url.searchParams.get('room_name')
+console.log(userName, roomName);
+
+if (!userName || !roomName) {
+  location.href = '/main/main.html'
+}
+
 // 1. 建立 socket 連線
 const clientIo = io()
 
