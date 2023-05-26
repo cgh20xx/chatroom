b45865dba7f64ac42e887dfab51ac4b709c4938c
Hank <cgh20xx@gmail.com>
2023-05-26 10:12:19 +0800

    feat: 新增 main 表單事件

--- src/client/pages/main/index.ts
+++ src/client/pages/main/index.ts
@@ -1,2 +1,13 @@
 import './index.css';
 
+// document.getElementById() 回傳預設型別會是 HTMLElement | null
+const inputName = document.getElementById('inputName') as HTMLInputElement
+const selectRoom = document.getElementById('selectRoom') as HTMLSelectElement
+const btnStart = document.getElementById('btnStart') as HTMLButtonElement
+
+btnStart.addEventListener('click', () => {
+  const userName = inputName.value
+  const roomName = selectRoom.value
+
+  console.log(userName, roomName);
+})
