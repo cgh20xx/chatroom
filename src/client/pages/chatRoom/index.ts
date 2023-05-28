1a80358b54b1dfa10dbd2e462e19889a7f9aa0d9
Hank <cgh20xx@gmail.com>
2023-05-28 11:21:40 +0800

    feat: 新增 返回按鈕功能

--- src/client/pages/chatRoom/index.ts
+++ src/client/pages/chatRoom/index.ts
@@ -18,6 +18,7 @@ const inputText = document.getElementById('inputText') as HTMLInputElement
 const btnSubmit = document.getElementById('btnSubmit') as HTMLInputElement
 const chatBoard = document.getElementById('chatBoard') as HTMLDivElement
 const headerRoomName = document.getElementById('headerRoomName') as HTMLParagraphElement
+const btnBack = document.getElementById('btnBack') as HTMLButtonElement
 // 用 as 斷言成指定型別，否則 getElementById 預設為 HTMLElement | null 的聯合型別，之後也無法正確提示屬性。
 
 headerRoomName.textContent = roomName
@@ -52,6 +53,10 @@ btnSubmit.addEventListener('click', (e) => {
   clientIo.emit('chat', text)
   // 清空輪入框
   inputText.value = ''
+});
+
+btnBack.addEventListener('click', (e) => {
+  location.href = '/main/main.html'
 });
 
 
