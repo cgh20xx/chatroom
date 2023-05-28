23b6c9f4a1dfe74172b4cf7189f0b4595ffed8ec
Hank <cgh20xx@gmail.com>
2023-05-28 10:13:47 +0800

    feat: 新增送出訊息功能

--- src/client/pages/chatRoom/index.ts
+++ src/client/pages/chatRoom/index.ts
@@ -11,6 +11,16 @@ if (!userName || !roomName) {
   location.href = '/main/main.html'
 }
 
+const inputText = document.getElementById('inputText') as HTMLInputElement
+const btnSubmit = document.getElementById('btnSubmit') as HTMLInputElement
+// 用 as 斷言成指定型別，否則 getElementById 預設為 HTMLElement | null 的聯合型別，之後也無法正確提示屬性。
+
+btnSubmit.addEventListener('click', (e) => {
+  const text = inputText.value
+  console.log('send:', text);
+});
+
+
 // 1. 建立 socket 連線
 const clientIo = io()
 
