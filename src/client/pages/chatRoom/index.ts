32953f94707d12b15faa5171330d088a165c2637
Hank <cgh20xx@gmail.com>
2023-05-28 10:53:15 +0800

    feat: 新增 前端聊天室畫面能正確顯示後端傳來的訊息

--- src/client/pages/chatRoom/index.ts
+++ src/client/pages/chatRoom/index.ts
@@ -16,7 +16,28 @@ if (!userName || !roomName) {
 
 const inputText = document.getElementById('inputText') as HTMLInputElement
 const btnSubmit = document.getElementById('btnSubmit') as HTMLInputElement
+const chatBoard = document.getElementById('chatBoard') as HTMLDivElement
 // 用 as 斷言成指定型別，否則 getElementById 預設為 HTMLElement | null 的聯合型別，之後也無法正確提示屬性。
+
+function msgHandler(msg: string) {
+  
+  const msgBox = document.createElement('div')
+  msgBox.classList.add('flex', 'justify-end', 'mb-4', 'items-end')
+  msgBox.innerHTML = 
+  `<div class="flex justify-end mb-4 items-end">
+    <p class="text-xs text-gray-700 mr-4">00:00</p>
+    <div>
+      <p class="text-xs text-white mb-1 text-right">hank</p>
+      <p
+        class="mx-w-[50%] break-all bg-white px-4 py-2 rounded-bl-full rounded-br-full rounded-tl-full"
+      >
+        ${msg}
+      </p>
+    </div>
+  </div>`
+  chatBoard.appendChild(msgBox)
+}
+
 
 btnSubmit.addEventListener('click', (e) => {
   const text = inputText.value
@@ -27,11 +48,12 @@ btnSubmit.addEventListener('click', (e) => {
 
 
 // 偵聽後端來的 join 事件
-clientIo.on('join', (msg) => {
+clientIo.on('join', (msg: string) => {
   console.log('join from server:', msg);
 })
 
 // 偵聽後端來的 chat 事件
-clientIo.on('chat', (msg) => {
+clientIo.on('chat', (msg: string) => {
   console.log('msg from server:', msg);
+  msgHandler(msg);
 })
