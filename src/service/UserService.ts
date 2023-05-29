01f7b68f0622043c568fcc7eb5412b5204b7f8a1
Hank <cgh20xx@gmail.com>
2023-05-29 11:06:25 +0800

    feat: 新增使用者管理(新增、查詢、刪除)

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -6,10 +6,28 @@ type UserData = {
 }
 
 export default class UserService {
-  // 記錄使用者的資訊
+  // 記錄所有使用者的資訊
   private userMap: Map<string, UserData>
 
   constructor() {
     this.userMap = new Map()
   }
+
+  addUser(userData: UserData) {
+    this.userMap.set(userData.id, userData)
+  }
+  
+  removeUser(id: string) {
+    if (this.userMap.has(id)) {
+      this.userMap.delete(id)
+    }
+  }
+
+  getUser(id: string) {
+    if (!this.userMap.has(id)) return null;
+
+    const user = this.userMap.get(id)
+    return user || null
+  }
+
 }
